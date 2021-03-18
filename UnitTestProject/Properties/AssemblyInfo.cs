7aa3e9dce06ca699cd963cceb7d29257991dbf03
GEHA-dk <gert.hansen@wsa.com>
2021-03-18 11:03:26 +0100

    GEHA

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fca7f8a3-6852-4c46-ae67-f924ddf4ce66")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/WorkerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkerLibrary;
+using System.Threading;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class WorkerTests
+    {
+        [TestMethod]
+        public async Task DoWork_RunToCompletion_ReturnsWorkDone()
+        {
+            CancellationToken ct = new CancellationToken();
+            Worker worker = new Worker();
+            Assert.AreNotEqual("Work Done!", await worker.DoWork(ct));
+        }
+    }
+}
